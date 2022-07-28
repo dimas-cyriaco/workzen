019f515ba097ebd1cc2b6676acd342ce95318780
Dimas Cyriaco <dimascyriaco@pm.me>
2022-07-28 12:22:12 -0300

    Update main.ts (#4)

--- apps/homepage/src/main.ts
+++ apps/homepage/src/main.ts
@@ -5,5 +5,6 @@ const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
+  <p>Updated!</p>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;

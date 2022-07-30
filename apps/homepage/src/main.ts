5f1b7917cd5dec0d95cd8a69316835b54f4304ff
Dimas Cyriaco <dimascyriaco@pm.me>
2022-07-30 19:02:34 -0300

    chore: corrige prettier (#5)

--- apps/homepage/src/main.ts
+++ apps/homepage/src/main.ts
@@ -1,10 +1,10 @@
-import './style.css';
+import './style.css'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const app = document.querySelector<HTMLDivElement>('#app')!;
+const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
   <p>Updated!</p>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+`

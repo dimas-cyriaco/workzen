3f35bf384a55715f55d34924821ef0f8f7386449
Dimas Cyriaco <dimascyriaco@pm.me>
2022-07-31 12:58:43 -0300

    chore: adiciona AWS_REGION no github

--- infra/index.ts
+++ infra/index.ts
@@ -1,4 +1,5 @@
 import * as pulumi from '@pulumi/pulumi'
+import * as github from '@pulumi/github'
 
 import GithubDeployer from './components/GithubDeployer'
 import ApplicationDeploymentBucket from './components/application-deployment-bucket'
@@ -16,6 +17,20 @@ const homepageBucket = new ApplicationDeploymentBucket(
   {
     applicationName: `homepage_${stack}`,
     sourceFolder: '../dist/apps/homepage',
+  }
+)
+
+
+github.getActionsPublicKey({
+  repository: 'workzen',
+})
+
+new github.ActionsSecret(
+  'aws-region',
+  {
+    repository: 'workzen',
+    secretName: 'AWS_REGION',
+    plaintextValue: 'sa-east-1',
   }
 )
 
